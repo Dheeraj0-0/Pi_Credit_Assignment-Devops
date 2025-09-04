a910f3175175af94a5b2e1808450a17131dd8022
Dheeraj Parihar <117371624+Dheeraj0-0@users.noreply.github.com>
2025-09-04 12:10:06 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,24 @@
 
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+  default     = "MyWebAppProject-RG"
+}
+
+variable "location" {
+  description = "The Azure region where resources will be created."
+  type        = string
+  default     = "East US"
+}
+
+variable "db_admin_login" {
+  description = "The administrator login for the PostgreSQL server."
+  type        = string
+  default     = "psqladmin"
+}
+
+variable "db_password" {
+  description = "The administrator password for the PostgreSQL server."
+  type        = string
+  sensitive   = true # Marks this as a secret
+}
