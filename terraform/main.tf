65854c958a80de4263f24a6406873e4d8876ea9f
Dheeraj Parihar <117371624+Dheeraj0-0@users.noreply.github.com>
2025-09-04 12:10:52 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,39 @@
 
+# Configure the Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a central resource group for the project
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Networking Module
+module "vpc" {
+  source              = "./modules/vpc"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+# Security Module
+module "security" {
+  source              = "./modules/security"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  app_subnet_id       = module.vpc.app_subnet_id
+  db_subnet_id        = module.vpc.db_subnet_id
+}
+
+# Application Infrastructure Module
+module "app" {
+  source                = "./modules/app"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  db_admin_login        = var.db_admin_login
+  db_password           = var.db_password
+  app_subnet_id         = module.vpc.app_subnet_id
+  db_subnet_id          = module.vpc.db_subnet_id
+  db_security_group_id  = module.security.db_nsg_id
+}
